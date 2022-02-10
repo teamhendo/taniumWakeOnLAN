b200cf191a3a06a7738f5d6f10f1f0f968f63ce8
Brent Henderson <10964176+teamhendo@users.noreply.github.com>
2022-02-09 20:16:31 -0500

    Initial upload of prerequisite content.
    Initial upload of the prerequisite content for the Tanium Wake-on-LAN article on buildbreakautomate.com.

---
+++ Send-Packet.ps1
@@ -0,0 +1,69 @@
+﻿#######################################################
+##
+## Adapted by Ammaar Limbada
+## Original Author: Matthijs ten Seldam, Microsoft (see: http://blogs.technet.com/matthts)
+##
+#######################################################
+ 
+<#
+.SYNOPSIS
+Starts a list of physical machines by using Wake On LAN.
+ 
+.DESCRIPTION
+Wake sends a Wake On LAN magic packet to a given machine's MAC address.
+ 
+.PARAMETER MacAddress
+MacAddress of target machine to wake.
+ 
+.EXAMPLE
+Wake A0DEF169BE02
+ 
+.INPUTS
+None
+ 
+.OUTPUTS
+None
+ 
+.NOTES
+Make sure the MAC addresses supplied don't contain "-" or ".".
+#>
+
+function Send-Packet([string]$MacAddress)
+{
+    <#
+    .SYNOPSIS
+    Sends a number of magic packets using UDP broadcast.
+ 
+    .DESCRIPTION
+    Send-Packet sends a specified number of magic packets to a MAC address in order to wake up the machine.  
+ 
+    .PARAMETER MacAddress
+    The MAC address of the machine to wake up.
+    #>
+ 
+    try
+    {
+        $Broadcast = ([System.Net.IPAddress]::Broadcast)
+ 
+        ## Create UDP client instance
+        $UdpClient = New-Object Net.Sockets.UdpClient
+ 
+        ## Create IP endpoints for each port
+        $IPEndPoint = New-Object Net.IPEndPoint $Broadcast, 9
+ 
+        ## Construct physical address instance for the MAC address of the machine (string to byte array)
+        $MAC = [Net.NetworkInformation.PhysicalAddress]::Parse($MacAddress.ToUpper())
+ 
+        ## Construct the Magic Packet frame
+        $Packet =  [Byte[]](,0xFF*6)+($MAC.GetAddressBytes()*16)
+ 
+        ## Broadcast UDP packets to the IP endpoint of the machine
+        $UdpClient.Send($Packet, $Packet.Length, $IPEndPoint) | Out-Null
+        $UdpClient.Close()
+    }
+    catch
+    {
+        $UdpClient.Dispose()
+        $Error | Write-Error;
+    }
+}
---
+++ wakeController.ps1
@@ -0,0 +1,63 @@
+﻿<#
+.SYNOPSIS
+	This script sends Magic Packets to one or many MAC addresses depending on the $TargetMAC variable. A value of FF:FF:FF:FF:FF:FF sends the packet to all ARP entries that are known
+    to be in an Incomplete, Stale, or Unreachable state.  Only one machine will be targeted with a Magic Packet broadcast if any other MAC address value is provided.
+	# LICENSE #
+	Copyright (C) 2022 - Brent Henderson
+	This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+	You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+#>
+
+[cmdletbinding()]
+param([string] $TargetMAC = $null)
+
+#Requires -Version 3.0
+
+# Script directory preamble
+
+if (Test-Path -LiteralPath 'variable:HostInvocation') 
+{ 
+    $InvocationInfo = $HostInvocation 
+    [string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+} 
+else 
+{ 
+    $InvocationInfo = $MyInvocation 
+    [string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+}
+
+# Decode encoded string passed via Tanium Package 
+
+$TargetMAC = [System.Uri]::UnescapeDataString("$TargetMAC")
+
+# Dot-source Send-Packet prerequisite and validate that it is present
+
+. "$scriptDirectory\Send-Packet.ps1"
+
+if ([bool]$(Get-Command -Name Send-Packet) -eq $false) 
+{
+    Write-Output 'Send-Packet is a mandatory prerequisite and was not detected.  Please ensure that the Send-Packet.ps1 file is included in the Tanium Package.'
+    exit
+}
+
+# Send Magic Packets to different audiences depending on the value of $TargetMAC
+
+if ($TargetMAC -eq 'FF:FF:FF:FF:FF:FF') 
+{
+    $netNeighbors = Get-NetNeighbor -AddressFamily IPv4 -State Incomplete,Stale,Unreachable | `
+                Where-Object {$null -ne $_.LinkLayerAddress -and '' -ne $_.LinkLayerAddress} | `
+                Select-Object -Property LinkLayerAddress -Unique 
+
+    foreach ($neighbor in $netNeighbors) 
+    {
+        Write-Output "Sending Magic Packet to $($neighbor.LinkLayerAddress)."
+        
+        Send-Packet -MacAddress "$($neighbor.LinkLayerAddress)"
+
+        Start-Sleep -Seconds (Get-Random -Minimum 3 -Maximum 15)
+    }
+}
+else 
+{
+    Send-Packet -MacAddress $TargetMAC
+}
